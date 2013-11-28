6aed5c38f3c9a08b217b1c25dfaf83646786b238
Ilya Gerasimov <oma9512@yandex.ru>
2013-11-28 20:58:14 +0400

    refact

--- twping/classes/exception.class.php
+++
@@ -1,7 +0,0 @@
-<?php defined('DATALIFEENGINE') or die('No direct script access.');
-/**
- * Author: Gerasimov Ilya (hip)
- * Github: https://github.com/Omashu/dle-twping
- */
-
-class Twping_Exception extends Exception {}
